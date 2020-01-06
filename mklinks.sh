3672f883eebc1c57c21a56f64c99f97704746656
itfin <itfin@qq.com>
2020-01-06 22:41:16 +0800

    fix bug in mklinks.sh

--- mklinks.sh
+++ mklinks.sh
@@ -1,6 +1,6 @@
 unlink ../tick.q ; ln -s Tx/core/tick.q ../
 unlink ../tick ; ln -s Tx/tick ../
-unlink tick/api.q ; ln -s core/api.q tick/
+unlink tick/api.q ; ln -s ../core/api.q tick/
 find ../l64 -type l -exec unlink {} \;  ;  ln -s ../Tx/ext/dll/l64/* ../l64/
 
 
